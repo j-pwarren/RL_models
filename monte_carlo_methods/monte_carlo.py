1acc6775bdfddee73a28cb8d28dbdadd6c031c43
James <jamespwarren83@gmail.com>
2020-01-16 17:57:48 +0000

    renamed the monte_carlo.py file

---
+++ monte_carlo_methods/monte_carlo.py
@@ -0,0 +1,45 @@
+import numpy as np
+import gym
+
+# environment_name = 'Blackjack-v0'
+# env = gym.make(environment_name)
+#
+# nS = env.observation_space.n
+# nA = env.action_space.n
+
+def run_episode(env):
+
+    state = env.reset()
+
+    return
+
+
+def make_epsilon_greedy_policy(Q, epsilon, nA):
+    """
+    Creates an epsilon-greedy policy based on a given Q-function and epsilon.
+
+    Args:
+        Q: A dictionary that maps from state -> action-values.
+            Each value is a numpy array of length nA (see below)
+        epsilon: The probability to select a random action . float between 0 and 1.
+        nA: Number of actions in the environment.
+
+    Returns:
+        A function that takes the observation as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+
+    """
+
+    def policy_fn(observation):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        best_action = np.argmax(Q[observation])
+        A[best_action] += (1.0 - epsilon)
+        return A
+
+    return policy_fn
+
+
+
+
+
+
